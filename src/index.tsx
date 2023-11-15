debbfc3d5fa342c8fd9670fe41f7f135e1c11c70
mahi07singh <mohit.diginto@gmail.com>
2023-11-15 12:54:01 +0530

    new changes

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ import './App.css';
 
 ReactDOM.render(
   <BrowserRouter>
-  <Provider store={store}>
+  {/* <Provider store={store}> */}
     <App />
-  </Provider>
+  {/* </Provider> */}
   </BrowserRouter>,
   document.getElementById("root") as HTMLElement
 );
